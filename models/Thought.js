7698826293f07c8c6a94f2ed8eaf88a83d86548b
eric <erictranchell@yahoo.com>
2023-07-12 17:00:24 -0400

    thoughts.js

---
+++ models/Thought.js
@@ -0,0 +1,75 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+    },
+    {
+        toJSON: {
+            getters: true,  
+        },
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+
+        // use reactionSchema to validate data for a reply
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// get total count of reactions and replies on retrieval
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+}
+);
+
+// create the Thought model using the thoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
+
